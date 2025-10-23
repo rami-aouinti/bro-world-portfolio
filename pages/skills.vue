3249de9b710bb8add48c687048f294f595f85133
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-10-24 01:43:09 +0200

    Initial commit (local reset)

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,5 +1,9 @@
 <template>
-  <LayoutLine />
-  <SectionSkills />
-  <LayoutLine />
+  <Line />
+  <Skills />
+  <Line />
 </template>
+<script setup lang="ts">
+import Line from "~/components/Layout/Line.vue";
+import Skills from "~/components/section/Skills.vue";
+</script>
