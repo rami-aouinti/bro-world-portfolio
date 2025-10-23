cfb81e31658b9e205c5c12a7c8068da9ccc03c8b
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-10-24 01:45:42 +0200

    Initial commit (local reset)

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { LOCALIZED_PAGE_META } from '~/utils/i18n/routes'
-
+import Line from "~/components/Layout/Line.vue";
+import Skills from "~/components/section/Skills.vue";
 definePageMeta(LOCALIZED_PAGE_META.skills)
 </script>
 
@@ -9,7 +10,3 @@ definePageMeta(LOCALIZED_PAGE_META.skills)
   <Skills />
   <Line />
 </template>
-<script setup lang="ts">
-import Line from "~/components/Layout/Line.vue";
-import Skills from "~/components/section/Skills.vue";
-</script>
