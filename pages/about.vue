bdb41d508ba98b1dae6c1ee04e13429be8a844a4
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-10-24 01:54:58 +0200

    Initial commit (local reset)

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { LOCALIZED_PAGE_META } from '~/utils/i18n/routes'
-
+import Line from "~/components/Layout/Line.vue";
+import Skills from "~/pages/skills.vue";
+import Experience from "~/pages/experience.vue";
+import Education from "~/pages/education.vue";
+import Cta from "~/components/section/Cta.vue";
+import About from "~/components/section/About.vue";
 definePageMeta(LOCALIZED_PAGE_META.about)
 </script>
 
@@ -14,11 +19,3 @@ definePageMeta(LOCALIZED_PAGE_META.about)
   <Education />
   <Cta />
 </template>
-<script setup lang="ts">
-import Line from "~/components/Layout/Line.vue";
-import Skills from "~/pages/skills.vue";
-import Experience from "~/pages/experience.vue";
-import Education from "~/pages/education.vue";
-import Cta from "~/components/section/Cta.vue";
-import About from "~/components/section/About.vue";
-</script>
