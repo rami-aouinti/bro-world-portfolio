5736d6be30ee709e4358e23cca4b345e77660db6
Rami <rami.aouinti@gmail.com>
2025-10-24 02:22:20 +0200

    fix: avoid importing page components

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { LOCALIZED_PAGE_META } from '~/utils/i18n/routes'
 import Line from "~/components/Layout/Line.vue";
-import Skills from "~/pages/skills.vue";
-import Experience from "~/pages/experience.vue";
-import Education from "~/pages/education.vue";
+import Skills from "~/components/section/Skills.vue";
+import Experience from "~/components/section/Experience.vue";
+import Education from "~/components/section/Education.vue";
 import Cta from "~/components/section/Cta.vue";
 import About from "~/components/section/About.vue";
 definePageMeta(LOCALIZED_PAGE_META.about)
