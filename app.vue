278a2ff7aa0565aaacab783f8f0dd22cfd3b3537
Rami <rami.aouinti@gmail.com>
2025-10-29 03:56:24 +0100

    Ensure suspense fallbacks render client-side

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <Suspense :suspensible="false">
+    <template #default>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </template>
+    <template #fallback>
+      <AppLoader />
+    </template>
+  </Suspense>
 </template>
